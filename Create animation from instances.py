f5a54ff9d5f1565e205edbe04fdff92fcdbaa451
Artengar <contact@artengar.com>
2017-10-31 12:11:45 +0100

    Two scripts I use in my teaching classes
    Two scripts I use in my teaching classes

---
+++ Create animation from instances.py
@@ -0,0 +1,70 @@
+#NOTE: this script is written for Drawbot within Glyphs!
+#NOTE: this scritp is suited for Glyphs animations on OpenType variations trough pre-defined instances. The design has to be adapted in order to create a loop.
+# For questions, contact Maarten Renckens
+
+from GlyphsApp import *
+import objc
+GSInstance = objc.lookUpClass("GSInstance")
+GSInterpolationFontProxy = objc.lookUpClass("GSInterpolationFontProxy")
+
+#https://forum.glyphsapp.com/t/drawing-instances-with-drawbot-plugin/5189/6
+
+# General document information
+ActiveFont = Glyphs.font
+selectedLayers = ActiveFont.selectedLayers
+pages = len(ActiveFont.instances)
+pageHeight = 1000
+pageWidth = 1000
+canvasWidth = 0
+canvasHeight = 0
+
+#provide general information in the log:
+print('For the font "%s" I can draw:')%ActiveFont.familyName
+
+#Decompose the components
+for thisLayer in selectedLayers:
+    currentGlyph = thisLayer.parent
+    for master in ActiveFont.masters:
+        if len(ActiveFont.glyphs[currentGlyph.name].layers[master.id].components) > 0:
+         ActiveFont.glyphs[currentGlyph.name].layers[master.id].decomposeComponents()
+
+#Create the different frames
+page=0
+for instance in ActiveFont.instances:
+    page+=1
+    if page == pages+1:
+        break
+    print("processing page %s")%page
+    newPage(pageWidth, pageHeight)
+    
+    fill(0, 0, 0, 1)
+    frameDuration(1/20)
+    #width and heigth only works after a page is created.
+    canvasWidth = width()
+    canvasHeight = height()
+    translate(canvasWidth/2-500, canvasHeight/2-300)
+    
+    
+    instanceFont = instance.interpolatedFontProxy
+    for thisLayer in selectedLayers:
+        instanceGlyph = instanceFont.glyphForName_(thisLayer.parent.name)
+        instanceLayer = instanceGlyph.layers[instanceFont.fontMasterID()]
+        drawPath(instanceLayer.bezierPath)
+    
+    
+    #instance2 = GSInstance()
+    #instance2.weightValue = 100
+    #instance2.widthValue = 100
+    #instance2.customValue = 100
+    #instance2.setFont_(ActiveFont)
+    #instanceFont = GSInterpolationFontProxy.alloc().initWithFont_instance_(ActiveFont, instance2)
+    #instanceGlyph = instanceFont.glyphs["Kaleidoscoop-2"]
+    #instanceLayer = instanceGlyph.layers[instanceFont.fontMasterID()]
+    #drawPath(instanceLayer.bezierPath)
+    #instance2 = BezierPath()
+    #instance2.text('G', font='Helvetica', fontSize=72, offset=(100,100) )
+    #drawPath(instance2)
+
+saveToPath = "~/Desktop/" + ActiveFont.familyName + ".gif"
+saveImage(saveToPath)
+print("ready!")
---
+++ Create animation from selected glyphs.py
@@ -0,0 +1,41 @@
+#NOTE: this script is written for Drawbot within Glyphs!
+#NOTE: this scritp is suited for Glyphs animations trough different selected glyphs.
+# For questions, contact Maarten Renckens
+
+from GlyphsApp import *
+
+# General document information
+ActiveFont = Glyphs.font
+selectedLayers = ActiveFont.selectedLayers
+widestFrame = 0
+
+#provide general information in the log:
+print('For the font "%s":')%ActiveFont.familyName
+
+#prepare the canvas:
+#get the widest frame:
+for thisLayer in selectedLayers:
+    if thisLayer.width > widestFrame:
+        widestFrame = thisLayer.width
+print "The widest frame I found was %s"%widestFrame
+#determine the canvas size:
+pageHeight = ActiveFont.upm + 400
+pageWidth = widestFrame + 400
+canvasWidth = 0
+canvasHeight = 0
+
+#Decompose the components
+for thisLayer in selectedLayers:
+    newPage(pageWidth, pageHeight)
+    fill(0, 0, 0, 1)
+    frameDuration(1/6)
+    #width and heigth only works after a page is created.
+    canvasWidth = width()
+    canvasHeight = height()
+    translate(canvasWidth/2-500, canvasHeight/2-300)
+    drawPath(thisLayer.bezierPath)
+
+saveToPath = "~/Desktop/" + ActiveFont.familyName + ".gif"
+saveImage(saveToPath)
+print("ready!")
+
